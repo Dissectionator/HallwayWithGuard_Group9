bf3e37e6b4a04c35c375192c4fe33a89fc411d53
danieldra910 <daniel_arias2000@knights.ucf.edu>
2022-02-13 22:37:35 -0500

    InitialEnemyAI

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,106 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.SceneManagement;
 
 public class EnemyController : MonoBehaviour
 {
-    [SerializeField] private Transform target;
-    public float attackRadius = 20f;
+    [SerializeField] private Transform movePositionTransform;
 
     private NavMeshAgent navMeshAgent;
-    
-    public Transform [] destinations;
-
-    float timer;
-
-    float maxTime = 3f;
-
-    private int currentPoint=0;
-
-    bool inRange = false;
-    public AudioSource musicSource;
-    public AudioClip musicClipOne;
-    Animator anim;
 
     void Awake()
     {
-       navMeshAgent=GetComponent<NavMeshAgent>();
-       currentPoint = 0;
-
+        navMeshAgent=GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-
-
-    float disTo = Vector3.Distance(transform.position, target.position);
-
-        if (disTo <= attackRadius)
-        {
-            timer+=Time.deltaTime;
-
-            if(timer > maxTime)
-            {
-                inRange = true;
-                
-                musicSource.clip = musicClipOne;
-                musicSource.Play();
-
-                navMeshAgent.speed =35;
-
-                transform.LookAt(target);
-
-                Vector3 moveTo = Vector3.MoveTowards(current: transform.position, target.position,maxDistanceDelta:100f);
-                navMeshAgent.destination = moveTo;
-                //navMeshAgent.destination = target.position;
-                
-            }
-
-        }
-        else
-        {
-            musicSource.Stop();
-            inRange = false;
-
-            BackToPath();
-
-        }
-
+        navMeshAgent.destination=movePositionTransform.position;
     }
-
-    void BackToPath()
-    {
-        if(inRange == false && navMeshAgent.remainingDistance <2f)
-        {
-            navMeshAgent.speed = 15;
-            navMeshAgent.destination = destinations [currentPoint].position;
-            UpdateCurrentPoint();
-        }
-    }
-
-    void UpdateCurrentPoint()
-    {
-        if(currentPoint == destinations.Length-1)
-        {
-            currentPoint = 0;
-        }
-        else
-        {
-            currentPoint ++ ;
-        }
-    
-    }
-
-    //Lose Condition
-    private void OnCollisionEnter(Collision other)
-    {
-        if(other.gameObject.CompareTag("Player"))
-        {
-            SceneManager.LoadScene("LoseScreen");
-        }
-    }
-
-   
 }
