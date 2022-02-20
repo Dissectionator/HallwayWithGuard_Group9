d6b8d6db8c33a396e1e429dc17f1f9b755317341
danieldra910 <daniel_arias2000@knights.ucf.edu>
2022-02-20 17:32:19 -0500

    esc goes to main menu

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraController : MonoBehaviour
 {
@@ -15,7 +16,7 @@ public class CameraController : MonoBehaviour
     {
         if(Input.GetKeyDown("escape"))
         {
-            Application.Quit();
+            SceneManager.LoadScene("MainMenu");
         }
     }
 }
