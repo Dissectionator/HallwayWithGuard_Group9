2e8363813e44c36092c077dace1da0624de0c6aa
Nikolai Laue <86926220+Wroofer@users.noreply.github.com>
2022-02-27 18:58:55 -0500

    fixed issues and combines versions

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    public float attackRadius = 20f;
+
+    private NavMeshAgent navMeshAgent;
+    
+    public Transform [] destinations;
+
+    float timer;
+
+    float maxTime = 3f;
+
+    private int currentPoint=0;
+
+    bool inRange = false;
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+
+    void Awake()
+    {
+       navMeshAgent=GetComponent<NavMeshAgent>();
+       currentPoint = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float disTo = Vector3.Distance(transform.position, target.position);
+
+        if (disTo <= attackRadius)
+        {
+            timer+=Time.deltaTime;
+
+            if(timer > maxTime)
+            {
+                inRange = true;
+                
+                musicSource.clip = musicClipOne;
+                musicSource.Play();
+
+                navMeshAgent.speed =8;
+
+                transform.LookAt(target);
+
+                Vector3 moveTo = Vector3.MoveTowards(current: transform.position, target.position,maxDistanceDelta:100f);
+                navMeshAgent.destination = moveTo;
+                //navMeshAgent.destination = target.position;
+                
+            }
+
+        }
+        else
+        {
+            musicSource.Stop();
+            inRange = false;
+            BackToPath();
+        }
+
+    }
+
+    void BackToPath()
+    {
+        if(inRange == false && navMeshAgent.remainingDistance <2f)
+        {
+            navMeshAgent.speed = 3;
+            navMeshAgent.destination = destinations [currentPoint].position;
+            UpdateCurrentPoint();
+        }
+    }
+
+    void UpdateCurrentPoint()
+    {
+        if(currentPoint == destinations.Length-1)
+        {
+            currentPoint = 0;
+        }
+        else
+        {
+            currentPoint ++ ;
+        }
+    
+    }
+
+    //Lose Condition
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("LoseScreen");
+        }
+    }
+
+   
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    
+    public float footSteps = 0.6f;
+    float footStepsTime;
+    public float speed=5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        footStepsTime=footSteps;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        //player movement
+        controller.Move(move*speed*Time.deltaTime);
+
+         if(x != 0 || z != 0 )
+        {
+            footSteps-=Time.deltaTime;
+            //play footsteps
+            if(footSteps <0 )
+            {
+                musicSource.clip = musicClipOne;
+                musicSource.Play();
+                footSteps=footStepsTime;
+            }
+        }
+        else
+        {
+            musicSource.Stop();
+        }
+        
+    }
+
+    // Win condition
+        private void OnTriggerEnter( Collider other)
+    {
+        if(other.gameObject.CompareTag("Pickup"))
+        {
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+            SceneManager.LoadScene("WinScreen");
+        }
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            SceneManager.LoadScene("LoseScreen");
+        }
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            SceneManager.LoadScene("LoseScreen");
+        }
+    }
+    
+    
+
+    
+}
